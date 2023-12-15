6b7c663a0bf769a3e32cc3fe1a509b220f7b15f8
Michal Pinos <xpinos03@stud.fit.vutbr.cz>
2023-12-15 16:03:36 +0100

    Clean some stuff

--- config.py
+++ config.py
@@ -11,9 +11,14 @@ Date: April 2023
 """
 
 import argparse
-import tensorflow as tf
 from functools import partial
+from absl import flags
 
+
+FLAGS=flags.FLAGS
+flags.DEFINE_string('logdir', 'logdir/', 'Logging directory')
+flags.DEFINE_string('load_weights', None, "Path to model weights.")
+flags.DEFINE_integer('gpus', 1, "Number of GPUs to use.")
 
 base_config = dict(
     #### Model Config ####
@@ -25,14 +30,14 @@ base_config = dict(
     init_channels=36,
     auxiliary=True,
     survival_prob=0.7,
-    train_image_size=112,
-    eval_image_size=112,
+    train_image_size=32,
+    eval_image_size=32,
     dataset="cifar10",
     train_dataset_size=50000,
     test_dataset_size=10000,
     
     #### Train Config ###
-    epochs=50,
+    epochs=1,
     optimizer="sgd",
     lr_sched="exponential",
     lr_init=0.025,
@@ -41,41 +46,47 @@ base_config = dict(
     lr_decay_factor=0.97,
     weight_decay=1e-5,
     label_smoothing=0.1,
-    train_batch_size=10,
+    train_batch_size=96,
     
     #### Evaluation Config ####
-    eval_batch_size=10,
+    eval_batch_size=96,
+    
+    #### Other ####
+    genotype=None,
+    logdir="logdir/",
+    gpus=1,
+    load_weights="",
 )
 
-class Config:
-    """Config class
-    """
-    def __init__(self, type):
-        """Method which initializes arguments parser
+# class Config:
+#     """Config class
+#     """
+#     def __init__(self, type):
+#         """Method which initializes arguments parser
 
-        """
-        parser = argparse.ArgumentParser('config', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-        parser.add_argument = partial(parser.add_argument, help=' ')
-        parser.add_argument('--seed', type=int, default=0, help='random seed')
-        parser.add_argument('--init_channels', type=int, default=16, help='num of init channels')
-        parser.add_argument('--learning_rate', type=float, default=0.025, help='learning rate')
-        parser.add_argument('--momentum', type=float, default=0.9, help='momentum')
-        parser.add_argument('--weight_decay', type=float, default=3e-4, help='weight decay')
-        parser.add_argument('--batch_size', type=int, default=32, help='batch size')
-        parser.add_argument('--epochs', type=int, default=50, help='number of epochs')
-        parser.add_argument('--arch_learning_rate', type=float, default=3e-4, help='learning rate for architecture')
-        parser.add_argument('--unrolled', action='store_true', default=False, help='use one step unrolled validation loss')
-        parser.add_argument('--cutout', action='store_true', default=False, help='use cutout on input images')
-        parser.add_argument('--cutout_length', type=int, default=16, help='cutout length')
-        parser.add_argument('--learning_rate_min', type=float, default=0.04, help='learning rate')
-        parser.add_argument('--layers', type=int, default=8, help='number of layers (sequential cells)')
-        parser.add_argument('--nodes', type=int, default=4, help='number of inner nodes (states)')
-        parser.add_argument('--multiplier', type=int, default=4, help='multiplier')
-        parser.add_argument('--grad_clip', type=int, default=5, help='gradient clipping')
-        parser.add_argument('--approx', action='store_true', default=False, help='use approx convolutions')
-        if type == 'train':
-            parser.add_argument('--auxiliary', action='store_true', default=False, help='use auxiliary head')
-            parser.add_argument('--genotype_file', required=True, help='genotype to build network from')
-            parser.add_argument('--drop_rate', type=float, default=0.2, help='dropout rate')
-            parser.add_argument('--auxiliary_weight', type=float, default=0.4, help='weight for auxiliary loss')
-        self.args = parser.parse_args()
+#         """
+#         parser = argparse.ArgumentParser('config', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+#         parser.add_argument = partial(parser.add_argument, help=' ')
+#         parser.add_argument('--seed', type=int, default=0, help='random seed')
+#         parser.add_argument('--init_channels', type=int, default=16, help='num of init channels')
+#         parser.add_argument('--learning_rate', type=float, default=0.025, help='learning rate')
+#         parser.add_argument('--momentum', type=float, default=0.9, help='momentum')
+#         parser.add_argument('--weight_decay', type=float, default=3e-4, help='weight decay')
+#         parser.add_argument('--batch_size', type=int, default=32, help='batch size')
+#         parser.add_argument('--epochs', type=int, default=50, help='number of epochs')
+#         parser.add_argument('--arch_learning_rate', type=float, default=3e-4, help='learning rate for architecture')
+#         parser.add_argument('--unrolled', action='store_true', default=False, help='use one step unrolled validation loss')
+#         parser.add_argument('--cutout', action='store_true', default=False, help='use cutout on input images')
+#         parser.add_argument('--cutout_length', type=int, default=16, help='cutout length')
+#         parser.add_argument('--learning_rate_min', type=float, default=0.04, help='learning rate')
+#         parser.add_argument('--layers', type=int, default=8, help='number of layers (sequential cells)')
+#         parser.add_argument('--nodes', type=int, default=4, help='number of inner nodes (states)')
+#         parser.add_argument('--multiplier', type=int, default=4, help='multiplier')
+#         parser.add_argument('--grad_clip', type=int, default=5, help='gradient clipping')
+#         parser.add_argument('--approx', action='store_true', default=False, help='use approx convolutions')
+#         if type == 'train':
+#             parser.add_argument('--auxiliary', action='store_true', default=False, help='use auxiliary head')
+#             parser.add_argument('--genotype_file', required=True, help='genotype to build network from')
+#             parser.add_argument('--drop_rate', type=float, default=0.2, help='dropout rate')
+#             parser.add_argument('--auxiliary_weight', type=float, default=0.4, help='weight for auxiliary loss')
+#         self.args = parser.parse_args()
---
+++ train_keras.py
@@ -0,0 +1,178 @@
+from datasets import CIFAR10Input
+import matplotlib.pyplot as plt
+import numpy as np
+from model_train import *
+import tensorflow as tf
+import re
+from absl import app
+
+from config import base_config as config
+from config import FLAGS as FLAGS
+import math
+import builtins
+import json
+
+
+
+physical_devices = tf.config.experimental.list_physical_devices('GPU')
+if len(physical_devices) > 0:
+    tf.config.experimental.set_memory_growth(physical_devices[0], True)
+
+
+class TrainableNetwork(Network):
+    
+    def __ini__(self, kwargs):
+        super().__init__(**kwargs)
+        
+    def _reg_l2_loss(self, weight_decay=1e-5, regex=r'.*(kernel|weight):0$'):
+        var_match = re.compile(regex)
+        return weight_decay * tf.add_n([
+            tf.nn.l2_loss(v)
+            for v in self.trainable_variables
+            if var_match.match(v.name)
+    ])
+    
+    def train_step(self, data):
+        features, labels = data
+        images, labels = features["image"], labels["label"]
+        with tf.GradientTape() as tape:
+            logits, logits_aux = self(images, training=True)
+            logits = tf.cast(logits, tf.float32)
+            
+            loss = self.compiled_loss(labels, logits,
+                                      regularization_losses=[self._reg_l2_loss()])
+            if self._auxiliary:
+                logits_aux  = tf.cast(logits_aux, tf.float32)
+                loss_aux = self.compiled_loss(labels, logits_aux)
+                loss += loss_aux
+                
+        self.optimizer.minimize(loss, self.trainable_weights, tape=tape)
+        self.compiled_metrics.update_state(labels, logits)
+        self.compiled_metrics.update_state(labels, logits_aux)
+        return {m.name: m.result() for m in self.metrics}
+
+    def test_step(self, data):
+        features, labels = data
+        images, labels = features["image"], labels["label"]
+        
+        logits, _ = self(images, training=False)
+        logits = tf.cast(logits, tf.float32)
+        
+        self.compiled_loss(labels, logits,
+                           regularization_losses=[self._reg_l2_loss()])
+        self.compiled_metrics.update_state(labels, logits)
+        return {m.name: m.result() for m in self.metrics}
+    
+
+
+
+def main(argv):
+        
+    with open("/home/michalpinos/darts_tf2/darts-tf2/genotypes/og", "r") as f:
+        genotype_str = f.read()
+    print(genotype_str)
+    genotype = builtins.eval(genotype_str)
+    
+    config["genotype"] = genotype_str
+    config["logdir"] = FLAGS.logdir
+    config["gpus"] = FLAGS.gpus
+    config["load_weights"] = FLAGS.load_weights    
+    ## dump used config, no modifications to config after this!
+    with open(FLAGS.logdir + "/config.json", "w") as f:
+        json.dump(config, f)
+    
+    # Set strategy
+    devices = [f"/device:gpu:{i}" for i in range(FLAGS.gpus)]
+    print(devices)
+    strategy = tf.distribute.MirroredStrategy(devices)
+    
+    with strategy.scope():
+        policy = tf.keras.mixed_precision.Policy("mixed_float16")
+        tf.keras.mixed_precision.set_global_policy(policy)    
+        print('Compute dtype: %s' % policy.compute_dtype)
+        print('Variable dtype: %s' % policy.variable_dtype)
+
+        model = TrainableNetwork(
+            config["init_channels"], None, config["num_classes"], config["num_layers"], 
+            n_nodes=4, multiplier=4, 
+            genotype=genotype, drop_rate=config["dropout_rate"], 
+            auxiliary=config["auxiliary"]
+            )
+
+        tb_callback = tf.keras.callbacks.TensorBoard(
+                log_dir=FLAGS.logdir)
+
+        model_checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(
+            filepath=FLAGS.logdir + "/weights.h5",
+            save_weights_only=True,
+            monitor='val_acc_top1',
+            mode='max',
+            save_best_only=True)
+        
+        
+        train_batch_size = config["train_batch_size"]
+        eval_batch_size = config["eval_batch_size"]
+        
+        # scale lr (initial_lr is set for batch_size 100)
+        #config["lr_init"] = config["lr_init"] * (train_batch_size/100.0)
+        
+        train_steps_per_epoch = config["train_dataset_size"]//train_batch_size
+        eval_steps_per_epoch = config["test_dataset_size"]//eval_batch_size
+        
+        decay_steps = train_steps_per_epoch * config["lr_decay_epoch"]
+        
+        if config["lr_sched"] == "exponential":
+            lr_scheduler = tf.keras.optimizers.schedules.ExponentialDecay(config["lr_init"], 
+                                                                        decay_steps, 
+                                                                        config["lr_decay_factor"], 
+                                                                        staircase=True
+                                                                        )
+        elif config["lr_sched"] == "cosine":
+            lr_scheduler = tf.keras.optimizers.schedules.CosineDecay(config["lr_init"], 
+                                                                    config["epochs"] * train_steps_per_epoch, 
+                                                                    config["lr_min"], 
+                                                                        )
+        else:
+            raise ValueError(f"Unknown lr_sched: '{config['lr_sched']}'!")
+
+        if config["optimizer"] == "adam":
+            optimizer = tf.keras.optimizers.Adam(lr_scheduler)
+        elif config["optimizer"] == "rmsprop":
+            optimizer = tf.keras.optimizers.RMSprop(lr_scheduler, rho=0.9, momentum=0.9, epsilon=1e-3)
+        elif config["optimizer"] == "sgd":
+            optimizer = tf.keras.optimizers.SGD(lr_scheduler)
+        else:
+            raise ValueError(f"Optimizer {config['optimizer']} not supported!")
+        
+        model.compile(optimizer=optimizer,
+                    loss=tf.keras.losses.CategoricalCrossentropy(from_logits=True, label_smoothing=config["label_smoothing"]),
+                    metrics=[
+                        tf.keras.metrics.TopKCategoricalAccuracy(k=1, name="acc_top1")
+                    ]
+                    )
+
+        model.summary()
+        model.build(input_shape=(None, config["train_image_size"], config["train_image_size"], 3))
+        
+        if FLAGS.load_weights:
+            model.load_weights(FLAGS.load_weights, by_name=True, skip_mismatch=True)
+            
+        
+        train = CIFAR10Input(True, image_size=config["train_image_size"], image_dtype=tf.float16, cache=False)
+        eval = CIFAR10Input(False, image_size=config["eval_image_size"], image_dtype=tf.float16, cache=False)
+        
+        ds_train = strategy.distribute_datasets_from_function(train.distribute_dataset_fn(train_batch_size))
+        ds_eval = strategy.distribute_datasets_from_function(eval.distribute_dataset_fn(eval_batch_size))
+        
+        model.fit(
+            ds_train,
+            epochs=config["epochs"],
+            steps_per_epoch=train_steps_per_epoch,
+            validation_data=ds_eval,
+            validation_steps=eval_steps_per_epoch,
+            verbose=1,
+            callbacks=[tb_callback, model_checkpoint_callback]
+        )
+    
+if __name__ == '__main__':
+  app.run(main)
